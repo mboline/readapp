03202cf9d908b434b80d2df9ae642a2daa79ecdb
mboline <mwboline@gmail.com>
2025-02-11 22:05:55 -0600

    Bind to 0.0.0.0 and use dynamic port

--- DecodingApp.py
+++ DecodingApp.py
@@ -43,4 +43,5 @@ def get_word_info():
         return jsonify({'error': 'An error occurred'}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
