5a66445a900a857af18438e55fac04b5c01670ae
mboline <mwboline@gmail.com>
2025-02-11 22:13:43 -0600

    Bind to the dynamic port provided by Render

--- DecodingApp.py
+++ DecodingApp.py
@@ -43,5 +43,5 @@ def get_word_info():
         return jsonify({'error': 'An error occurred'}), 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port, debug=True)
