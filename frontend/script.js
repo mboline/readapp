a2efe7f4b8dfb4bfd131a5dc97acb397b0b6e36e
Mark Boline <mwboline@gmail.com>
2025-02-17 19:40:47 -0600

    commented out line again

--- frontend/script.js
+++ frontend/script.js
@@ -109,7 +109,7 @@ document.getElementById('phonogram-search-form').addEventListener('submit', func
                 errorMessage.style.display = 'block';
             } else {
                 // Populate phonogram information
-                document.getElementById('phonogram-title').textContent = `Phonogram: ${data.phonogram}`;
+                //document.getElementById('phonogram-title').textContent = `Phonogram: ${data.phonogram}`;
                 document.getElementById('phonogram-explanation').textContent = data.sample_words;
 
                 // Handle audio
